bb16bb4712269342a3d20ca993b15a8b4e7e64f9
Reika <reikasminecraft@gmail.com>
2020-09-30 14:50:37 -0400

    Update 7

--- constants.lua
+++ constants.lua
@@ -1,5 +1,6 @@
 require "config"
 
+require "__DragonIndustries__.arrays"
 require "__DragonIndustries__.biomecolor"
 
 CHUNK_SIZE = 32
@@ -9,13 +10,16 @@ MIN_DISTANCE = 100
 DISTANCE_FULL = 500
 
 local TILE_COLOR_MAP = {}
-local EXTRA_MAP = {}
+local COLOR_GROUPS = {}
+--local EXTRA_MAP = {}
 
 local function addExtraValidColors(tile, name, list)
 	local extras = EXTRA_MAP[name]
 	if extras then
 		for _,c in pairs(extras) do
-			table.insert(list, c)
+			if not listHasValue(list, c) then
+				table.insert(list, c)
+			end
 		end
 	end
 end
@@ -25,8 +29,13 @@ function getMiasmaColorsForTile(tile)
 	if water then return nil end
 	local n = tile.name
 	if not TILE_COLOR_MAP[n] then
-		TILE_COLOR_MAP[n] = base
-		addExtraValidColors(tile, n, base)
+		--TILE_COLOR_MAP[n] = base
+		for name,group in pairs(COLOR_GROUPS) do
+			if string.find(n, name, 1, true) then
+				TILE_COLOR_MAP[n] = group
+			end
+		end
+		--addExtraValidColors(tile, n, base)
 	end
 	return util.table.deepcopy(TILE_COLOR_MAP[n])
 end
@@ -35,6 +44,7 @@ function initModifiers(isInit)
 
 end
 
+--[[
 local function addExtraColor(tile, color)
 	if not EXTRA_MAP[tile] then
 		EXTRA_MAP[tile] = {}
@@ -53,3 +63,17 @@ end
 addExtraColor("sand-1", "red")
 addExtraColor("sand-2", "red")
 addExtraColor("sand-3", "red")
+--]]
+
+local function addColorGroup(colors, names)
+	for _,n in pairs(names) do
+		COLOR_GROUPS[n] = colors
+	end
+end
+
+addColorGroup({"red", "orange", "yellow"}, {"red", "dustyrose", "orange", "brown", "desert", "sand", "dirt", "tan", "beige"})
+addColorGroup({"white", "green", "yellow"}, {"yellow", "cream", "olive"})
+addColorGroup({"argon", "green", "cyan"}, {"green", "grass"})
+addColorGroup({"white", "cyan", "argon"}, {"snow", "white", "ice", "frozen", "black", "grey", "gray"})
+addColorGroup({"argon", "blue", "purple"}, {"blue", "turqoise"})
+addColorGroup({"blue", "purple", "magenta"}, {"purple", "violet", "mauve", "aubergine"})
